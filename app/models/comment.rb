0038d0746324174d037c7736c7b0143bb50c0866
Anderson Junior <andersonsoares.jn@gmail.com>
2024-05-16 15:36:51 -0300

    doc: Add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
+  validates :description, :user, :post, presence: true
+  validates :description, length: {in 2..24}
+
   belongs_to :user
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, :description, :user, presence: true
+  validates :title, length: {in 4..12}
+  validates :description, length: {in 8..24}
+
   has_many :comments
   belongs_to :user
 end
