d31ce7f1b611af4c714ac1152356ce3021d7d879
Anderson Junior <andersonsoares.jn@gmail.com>
2024-05-16 15:42:48 -0300

    fix: Fix syntax error in validates {in:}

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   validates :description, :user, :post, presence: true
-  validates :description, length: {in 2..24}
+  validates :description, length: {in: 2..24}
 
   belongs_to :user
   belongs_to :post
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   validates :title, :description, :user, presence: true
-  validates :title, length: {in 4..12}
-  validates :description, length: {in 8..24}
+  validates :title, length: {in: 4..12}
+  validates :description, length: {in: 8..24}
 
   has_many :comments
   belongs_to :user
